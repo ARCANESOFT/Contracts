b950877b87554b54b77b0ed048039eeca8017e0f
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-21 19:43:36 +0000

    Adding the new Auth contract : PermissionsGroup

---
+++ src/Auth/Models/PermissionsGroup.php
@@ -0,0 +1,111 @@
+<?php namespace Arcanesoft\Contracts\Auth\Models;
+
+/**
+ * Interface  PermissionsGroup
+ *
+ * @package   Arcanesoft\Contracts\Auth\Models
+ * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+interface PermissionsGroup
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Relationships
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Permissions Groups has many permissions.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function permissions();
+
+    /* ------------------------------------------------------------------------------------------------
+     |  CRUD Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Create and attach a permission.
+     *
+     * @param  array  $attributes
+     * @param  bool   $reload
+     */
+    public function createPermission(array $attributes, $reload = true);
+
+    /**
+     * Attach the permission to a group.
+     *
+     * @param  Permission  $permission
+     * @param  bool        $reload
+     */
+    public function attachPermission(&$permission, $reload = true);
+
+    /**
+     * Attach the permission by id to a group.
+     *
+     * @param  int   $id
+     * @param  bool  $reload
+     *
+     * @return Permission
+     */
+    public function attachPermissionById($id, $reload = true);
+
+    /**
+     * Attach a collection of permissions to the group.
+     *
+     * @param  \Illuminate\Database\Eloquent\Collection|array  $permissions
+     * @param  bool                                            $reload
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|array
+     */
+    public function attachPermissions($permissions, $reload = true);
+
+    /**
+     * Attach the permission from a group.
+     *
+     * @param  Permission  $permission
+     * @param  bool        $reload
+     */
+    public function detachPermission(&$permission, $reload = true);
+
+    /**
+     * Attach the permission by id to a group.
+     *
+     * @param  int   $id
+     * @param  bool  $reload
+     *
+     * @return Permission
+     */
+    public function detachPermissionById($id, $reload = true);
+
+    /**
+     * Detach multiple permissions by ids.
+     *
+     * @param  array  $ids
+     * @param  bool   $reload
+     *
+     * @return int
+     */
+    public function detachPermissions(array $ids, $reload = true);
+
+    /**
+     * Detach all permissions from the group.
+     *
+     * @param  bool  $reload
+     *
+     * @return int
+     */
+    public function detachAllPermissions($reload = true);
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Check Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Check if role has the given permission (Permission Model or Id).
+     *
+     * @param  Permission|int  $id
+     *
+     * @return bool
+     */
+    public function hasPermission($id);
+}
